9926631053063c687ed1e1b0214f550f58687003
shrestha1 <newardipesh@gmail.com>
2023-04-04 17:31:54 +0545

    updated the LeNet architecture properly

--- objectClassification/LeNet/model.py
+++ objectClassification/LeNet/model.py
@@ -6,20 +6,30 @@ class LeNet(nn.Module):
     def __init__(self, in_ch, output) -> None:
         super(LeNet, self).__init__()
         self.model = nn.Sequential(
-
+            # layer 1
             nn.Conv2d(in_channels=in_ch, out_channels=6, kernel_size = 5),
             nn.Tanh(),
+            
+            # layer 2
             nn.AvgPool2d(kernel_size=2, stride=2),
             
+            # layer 3
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5),
             nn.Tanh(),
+            
+            # layer 4
             nn.AvgPool2d(kernel_size=2, stride=2),
-
-            nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5),
-            nn.Tanh(),
             nn.Flatten(),
+
+            # layer 5
+            nn.Linear(400, 120),
+            nn.ReLU(),
+
+            # layer 6
             nn.Linear(120, 84),
             nn.ReLU(),
+            
+            # layer 7
             nn.Linear(84, out_features= output)
         )
 
@@ -35,4 +45,10 @@ if __name__ == '__main__':
 
     y = model(x)
 
-    summary(model, (1,32,32))    
+    summary(model, (1, 32, 32))
+    # from torch.utils.tensorboard import SummaryWriter
+
+    # writer = SummaryWriter()
+    
+    # writer.add_graph(model, x)    
+    # writer.close()

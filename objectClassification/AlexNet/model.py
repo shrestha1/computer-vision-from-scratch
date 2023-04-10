3b68d46fad32ed082fdace0bf201e0bbdf8154a9
shrestha1 <newardipesh@gmail.com>
2023-04-10 07:51:16 +0545

    modified implementation of Alex net

--- objectClassification/AlexNet/model.py
+++ objectClassification/AlexNet/model.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
 
+############################
+#       AlexNet
+############################
+
 class AlexNet(nn.Module):
     def __init__(self, n_ch, out_ch, drop_out) -> None:
         super(AlexNet, self).__init__()
@@ -9,8 +13,38 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
 
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
 
+            nn.Conv2d(in_channels=256, out_channels=348, kernel_size=3),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(in_channels=348, out_channels=348, kernel_size=3),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(in_channels=348, out_channels=256, kernel_size=3),
+            nn.ReLU(inplace=True),
         )
+
+        self.fc_layer = nn.Sequential(
+            nn.Dropout(p=drop_out),
+            nn.Linear(256*5*5, 4096),
+            nn.ReLU(inplace=True),
+
+            nn.Dropout(p=drop_out),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(4096, out_ch)
+        )
+
+    def forward(self, x):
+        f = self.features(x)      #Extract the features
+        f = torch.flatten(f, 1)   #Flatten the tensor
+        return self.fc_layer(f)
+
+
 
 
 if __name__=='__main__':
@@ -18,4 +52,5 @@ if __name__=='__main__':
     x = torch.randn(1, 3, 227, 227)
     model = AlexNet(3, 10, 0.5)
     y = model(x)
-    
+
+    print(y)

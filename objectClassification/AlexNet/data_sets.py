8fd6ec3cd1ae48ead6db8f61d7750955eb52309a
shrestha1 <newardipesh@gmail.com>
2023-04-12 08:24:31 +0545

    minor update in datasets

--- objectClassification/AlexNet/data_sets.py
+++ objectClassification/AlexNet/data_sets.py
@@ -1,4 +1,7 @@
+import torch
 from torch.utils.data import Dataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 import data_utils as du
 import os
 import numpy as np
@@ -25,17 +28,17 @@ class TinyImageNet(Dataset):
     def __getitem__(self, index):
         img_file, label = self.data[index]
         root_and_dir = os.path.join(self.data_path, self.classes[label])
-        image = np.array(Image.open(os.path.join(root_and_dir, img_file)))
+        image = np.array(Image.open(os.path.join(root_and_dir, img_file)).convert("RGB"))
 
         if self.transform is not None:
-            augmentations = self.transform(image=image)
-            image = augmentations["image"]
-
+            image = self.transform(image)
+            # image = augmentations["image"]
+            
         return image, label
     
 if __name__ == '__main__':
     ## test data sets 
-    data_path = './data/tiny-imagenet-200/train'
+    data_path = './data/tiny-imagenet-200/val'
     classes = du.valid_classes(data_path)
     classes_list = tuple(classes.keys())
     

48a89c20603d7f6174611cf9d5cfff609294a527
shrestha1 <newardipesh@gmail.com>
2023-04-25 13:40:39 +0545

    all vgg model expect vgg11LRN and vgg11 conv1

--- objectClassification/VGG/model.py
+++ objectClassification/VGG/model.py
@@ -2,30 +2,11 @@ import torch
 import torch.nn as nn
 
 
-class VGG11(nn.Module):
-    def __init__(self, n_ch, out_ch):
-        
-        __n_layers = [64,'M', 128, 'M', 256, 256,'M', 512, 512,'M', 512, 512,'M']
-        
-        super(VGG11, self).__init__()
-        self.feature_list = []
-        
-        self.n_ch = n_ch
-        for ch in __n_layers:
-            if ch == 'M':
-                self.feature_list.append(nn.MaxPool2d(2, 2))
-            else:
-                self.feature_list.extend([nn.Conv2d(in_channels=self.n_ch,
-                                                     out_channels= ch, 
-                                                     kernel_size=3, 
-                                                     stride=1, 
-                                                     padding=1),
-                                          nn.ReLU(),
-                                    ])
-                self.n_ch = ch
-            
-        self.features = nn.Sequential(*self.feature_list)
+class VGG(nn.Module):
+    def __init__(self, config, out_ch=1000):
+        super(VGG, self).__init__()    
 
+        self.features = self.create_conv_layer(config=config)
         self.fc_layer = nn.Sequential(
             nn.Linear(512*7*7, 4096),
             nn.ReLU(),
@@ -41,18 +22,55 @@ class VGG11(nn.Module):
         ## flatten the f to match with fc layer
         f = torch.flatten(f, 1) 
         return self.fc_layer(f)
+    
+    def create_conv_layer(self, config):
+        in_channel = 3
+        layers = []
 
-
+        for c in config:
+            if c == 'M':
+                layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+            else:
+                layers.extend([
+                                nn.Conv2d(in_channels=in_channel,
+                                                     out_channels= c, 
+                                                     kernel_size=3, 
+                                                     stride=1, 
+                                                     padding=1),
+                                nn.ReLU(inplace=True)])
+                in_channel = c
+        return nn.Sequential(*layers)
+        
+    @staticmethod
+    def vgg11():
+        config = [64,'M', 128, 'M', 256, 256,'M', 512, 512,'M', 512, 512,'M']
+        return VGG(config)
+    
+    @staticmethod
+    def vgg13():
+        config = [64, 64,'M', 128, 128, 'M', 256, 256,'M', 512, 512,'M', 512, 512,'M']
+        return VGG(config)
+    
+    @staticmethod
+    def vgg16():
+        config = [64, 64,'M', 128, 128, 'M', 256, 256, 256,'M', 512, 512, 512,'M', 512, 512, 512,'M']
+        return VGG(config)
+    
+    @staticmethod
+    def vgg19():
+        config = [64, 64,'M', 128, 128, 'M', 256, 256, 256, 256,'M', 512, 512, 512, 512,'M', 512, 512, 512, 512,'M']
+        return VGG(config)
+    
 if __name__ == '__main__':
     # x = batch, channel , height, width
     x = torch.rand((1, 3, 224, 224))
 
-    model = VGG11(3, 1000)
+    model = VGG.vgg16()
 
     # print(model(x))
     from torchsummary import summary
     summary(model, (3, 224, 224))
     
     from torchvision import models
-    vgg = models.vgg11()
+    vgg = models.vgg16()
     summary(vgg, (3, 224, 224))
